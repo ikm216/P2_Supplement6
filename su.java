8a1afb49157b44cb32d690d406a07b98fcad5bf6
ikm216 <ikmclassof22@gmail.com>
2024-12-04 23:42:42 -0500

    doc:added documentation to the feature of generating a random list of integers using a size

--- su.java
+++ su.java
@@ -1,6 +1,12 @@
 import java.util.*;
 
 public class su{
+    /**
+ * Generates a random list of integers using a size.
+ *
+ * @param size The size of the random list to generate.
+ * @return A list of random integers.
+ */
     public static List<Integer> random_list_integers(int size){
         Random rand = new Random();
         List<Integer> rand_list = new ArrayList<>();
