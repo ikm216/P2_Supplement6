0faa0f896b9f8676447237df353d45c382667556
ikm216 <ikmclassof22@gmail.com>
2024-12-04 23:31:02 -0500

    test:added test to generate a random list of integers using a size

--- test_java.java
+++ test_java.java
@@ -1,3 +1,11 @@
+import java.util.*;
+import org.junit.*;
+
 public class test_java {
-    
+    @Test
+    public void test_should_return_a_random_list_integers(){
+        int size = 8;
+        List<Integer> test = su.random_list_integers(size);
+        Assert.assertEquals(size, test.size());
+    }
 }
