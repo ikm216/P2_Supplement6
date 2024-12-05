1252de22bf1b2f6359657f88882314285c48a63c
ikm216 <ikmclassof22@gmail.com>
2024-12-05 00:01:02 -0500

    test:added test to make a map of the occurences

--- test_java.java
+++ test_java.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import org.junit.*;
+import org.junit.jupiter.api.TestFactory;
 
 public class test_java {
     @Test
@@ -7,5 +8,14 @@ public class test_java {
         int size = 8;
         List<Integer> test = su.random_list_integers(size);
         Assert.assertEquals(size, test.size());
+    }
+
+    @Test
+    public void test_should_return_a_map_of_occurences(){
+        List<Integer> list = new ArrayList<>(Arrays.asList(2, 2, 4, 4, 4, 4, 3, 3, 3));
+        Map<Integer, Integer> test = su.mapOccurrences(list);
+        Assert.assertEquals(2, (int) test.get(2));
+        Assert.assertEquals(4, (int) test.get(4));
+        Assert.assertEquals(3, (int) test.get(3));
     }
 }
